d4be92f7dc7b3d1a5e2ac82a89eef8bf6cb161e0
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-09-29 11:47:19 -0300

    test(DwarfTest): corrigir casos antigos, adicionar caso numeroDaSorte

--- src/modulo-01-java-OO/modulo-01/DwarfTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarfTest.java
@@ -7,28 +7,32 @@ public class DwarfTest
 {
     @Test
     public void dwarfNasceCom110Vida() {
-        Dwarf gimli = new Dwarf();
+        DataTerceiraEra data = new DataTerceiraEra(12,03,5000);
+        Dwarf gimli = new Dwarf("Gimli",data);
         assertEquals(110, gimli.getVida());
     }
 
     @Test
     public void dwarfPerdeVidaUmaVez() {
-        Dwarf gimli = new Dwarf();
+        DataTerceiraEra data = new DataTerceiraEra(12,03,5000);
+        Dwarf gimli = new Dwarf("Gimli",data);
         gimli.perderVida();
         assertEquals(100, gimli.getVida());
     }
 
     @Test
     public void dwarfPerdeVidaDuasVezes() {
-        Dwarf gimli = new Dwarf();
+        DataTerceiraEra data = new DataTerceiraEra(12,03,5000);
+        Dwarf gimli = new Dwarf("Gimli",data);
         gimli.perderVida();
         gimli.perderVida();
         assertEquals(90, gimli.getVida());
     }
-
+    /*
     @Test
     public void dwarfPerdeVidaVinteVezes() {
-        Dwarf gimli = new Dwarf();
+        DataTerceiraEra data = new DataTerceiraEra(12,03,5000);
+        Dwarf gimli = new Dwarf("Gimli",data);
         gimli.perderVida();
         gimli.perderVida();
         gimli.perderVida();
@@ -50,6 +54,18 @@ public class DwarfTest
         gimli.perderVida();
         gimli.perderVida();
         assertEquals(-90, gimli.getVida());
+    }
+    */
+   
+   @Test
+    public void numeroDaSorteDwarfAnoBissextoNomeSeixas() {
+        DataTerceiraEra data = new DataTerceiraEra(20,12,2016);
+        Dwarf seixas = new Dwarf("Seixas",data);
+        
+        seixas.getNumeroDaSorte();
+        
+        assertEquals(110, seixas.getVida());
+        
     }
 }
 

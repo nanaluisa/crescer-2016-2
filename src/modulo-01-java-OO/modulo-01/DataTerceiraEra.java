9da0cd9585853af956dde1990030eb85508c7ab4
Unknown <annaluisa1703@gmail.com>
2016-09-28 23:29:13 -0300

    feat(DataTerceiraEra):
    Implementar método ehBissexto().

---
+++ src/modulo-01-java-OO/modulo-01/DataTerceiraEra.java
@@ -0,0 +1,35 @@
+public class DataTerceiraEra
+{
+    private int dia, mes, ano;
+    
+    public DataTerceiraEra(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    
+    public int getDia (){
+        return dia ;
+    }
+    
+    public int getMes (){
+        return mes ;
+    }
+    
+    public int getAno (){
+        return ano ;
+    }
+        
+    public boolean ehBissexto(){
+        if(ano % 4 == 0){
+           if(ano % 100 == 0){
+                if(ano % 400 == 0){
+                    return true;
+                }
+                return false;
+           }
+           return true;
+       }
+        return false;
+    }
+}

e83a666eb57b454a3e6861f54ca70f2033d6baa2
Anna Luísa <annaluisa1703@gmail.com>
2016-10-05 13:57:09 -0300

    test(ExercitoTest): implementar novos cenarios ao Elfos Alistados.

--- src/modulo-01-java-OO/modulo-01/ExercitoTest.java
+++ src/modulo-01-java-OO/modulo-01/ExercitoTest.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 public class ExercitoTest{
     @Test
-    public void alistar3ElfosVerdesE2Noturnos(){
+    public void alistar3ElfosVerdesE3Noturnos(){
         //Arrange
         Exercito milico = new Exercito();
         
@@ -25,5 +25,56 @@ public class ExercitoTest{
         
         //Assert
         assertEquals(6, milico.getElfosAlistados().size() );
+    }
+    
+    @Test
+    public void alistar3ElfosVerdesE2NoturnosETentarAlistarElfoNormal(){
+        //Arrange
+        Exercito milico = new Exercito();
+        
+        ElfoVerde green1 = new ElfoVerde("Green 1");
+        ElfoVerde green2 = new ElfoVerde("Green 2");
+        ElfoVerde green3 = new ElfoVerde("Green 3");
+        
+        ElfoNoturno nigth1 = new ElfoNoturno("Nigth 1");
+        ElfoNoturno nigth2 = new ElfoNoturno("Nigth 2");
+        
+        Elfo elfo1 = new Elfo("Elfo normal");
+        //Act
+        milico.addElfosValidos(green1);
+        milico.addElfosValidos(green2);
+        milico.addElfosValidos(green3);
+        milico.addElfosValidos(nigth1);
+        milico.addElfosValidos(nigth2);
+        milico.addElfosValidos(elfo1);
+        
+        
+        //Assert
+        assertEquals(5, milico.getElfosAlistados().size() );
+    }
+    
+    
+    @Test
+    public void tentarAlistar6ElfosNormais(){
+        //Arrange
+        Exercito milico = new Exercito();
+               
+        Elfo elfo1 = new Elfo("Elfo normal");
+        Elfo elfo2 = new Elfo("Elfo normal");
+        Elfo elfo3 = new Elfo("Elfo normal");
+        Elfo elfo4 = new Elfo("Elfo normal");
+        Elfo elfo5 = new Elfo("Elfo normal");
+        Elfo elfo6 = new Elfo("Elfo normal");
+        //Act
+        milico.addElfosValidos(elfo1);
+        milico.addElfosValidos(elfo2);
+        milico.addElfosValidos(elfo3);
+        milico.addElfosValidos(elfo4);
+        milico.addElfosValidos(elfo5);
+        milico.addElfosValidos(elfo6);
+        
+        
+        //Assert
+        assertEquals(0, milico.getElfosAlistados().size() );
     }
 }

64ace04b629e78f7d686a102666f04e380a8696f
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-10-05 11:54:48 -0300

    feat(Exercito): implementar class Exercito de Elfos.

---
+++ src/modulo-01-java-OO/modulo-01/Exercito.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+public class Exercito{
+    ArrayList<Elfo> elfosAlistados = new ArrayList<Elfo>();
+    private void addElfosValidos(Elfo elfo){
+         boolean podeAlistar;
+         boolean validos = elfo instanceof ElfoVerde || elfo instanceof ElfoNoturno;
+         //<condição> ? <operação 1> : <operação 2>;
+         podeAlistar = validos ? elfosAlistados.add(elfo) : false;
+    }
+    
+    private Elfo buscarElfoPorNome(String nomeProcurado){
+        boolean achei;
+        for(int i= 0; i < elfosAlistados.size();i++){
+            if(elfosAlistados.get(i).getNome().equals(nomeProcurado)){
+                Elfo elfoAchado = elfosAlistados.get(i);
+                return elfoAchado;
+            }
+        }
+        return null;
+    } 
+    
+    /*Crie um exército de Elfos que pode:
+       - Alistar um elfo, desde que ele seja um ElfoVerde ou ElfoNoturno.
+       - Buscar um elfo pelo nome.
+        OBS: exército é uma nova abstração no jogo, portanto crie uma nova...   CLASSE? LISTA?
+        OBS: quando alistamos alguém em um contingente quer dizer que aquela pessoa entrou na lista, ou seja...
+        
+        ------------------------------------------------------------------------
+        
+        Exercício 2
+
+        Pesquisando por Elfos
+        Crie uma forma de obter os elfos com determinado Status passado por parâmetro.
+        Sugestão de assinatura:
+        //ArrayList<Elfo> buscar(Status status)
+            */
+    public void adicionarItem(Item item) {         
+        String[] validas = getNomesValidos();
+        
+    }
+    //this.status = this.vida < 1 ? Status.MORTO : this.status;
+    private String[] getNomesValidos() { 
+        return new String[] {  
+            "Espada de aço valiriano",  
+            "Arco e Flecha de Vidro" 
+        }; 
+    } 
+    
+}
---
+++ src/modulo-01-java-OO/modulo-01/ExercitoTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class ExercitoTest{
+    @Test
+    public void alistar3ElfosVerdesE2Noturnos(){
+        //Arrange
+        Exercito milico = new Exercito();
+        
+        ElfoVerde green1 = new ElfoVerde("Green 1");
+        ElfoVerde green2 = new ElfoVerde("Green 2");
+        ElfoVerde green3 = new ElfoVerde("Green 3");
+        
+        ElfoNoturno nigth1 = new ElfoNoturno("Nigth 1");
+        ElfoNoturno nigth2 = new ElfoNoturno("Nigth 2");
+        ElfoNoturno nigth3 = new ElfoNoturno("Nigth 3");
+        //Act
+        //milico.addElfosValidos(green1);
+        
+        
+        //Assert
+    
+    }
+}

c69079a04a79d4021001bf4b83fa4447737ba024
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-10-25 09:59:35 -0200

    feat(ExerciciosLista1) : implementar exercicio 1, lista 1, gerarPiramide.

--- src/modulo-04-js/exercicios/lista1/exercicio1.js
+++ src/modulo-04-js/exercicios/lista1/exercicio1.js
@@ -1,44 +1,19 @@
-
+// . Pirâmide iluminatti
 //
-// function exibirNaTela(elfo){
-//
-//     function maiusculo(texto){
-//     return texto.toUpperase();
-//     }
-//
-
-
-// function gerarPiramide(niveis){
-//   var word =.join('R$');
-//   for(var i=0; i<word.length; i++){
-//     return console.log(word[i]);
-//   }
-// }
+// Escreva uma função gerarPiramide(niveis) que imprime com console.log uma pirâmide com uma quantidade de níveis informada por parâmetro, utilizando os caracteres R$. Ex:
+//   gerarPiramide(9);
+//   R$
+//   R$R$
+//   R$R$R$
+//   R$R$R$R$
+//   R$R$R$R$R$
+//   R$R$R$R$R$R$
+//   R$R$R$R$R$R$R$
+//   R$R$R$R$R$R$R$R$
+//   R$R$R$R$R$R$R$R$R$
 
 function gerarPiramide(niveis) {
-    for (var i = 0; i < niveis; i++) {
-      return Array(niveis).join('RS');
+    for (var i = 1; i <= niveis; i++) {
+        console.log('R$'.repeat(i));
+    }
 }
-console.log(gerarPiramide(niveis));
-
-
-
-//
-// for(var word = ''; word.length < 10; word += 'a'){}
-// If you need to repeat multiple chars, multiply your conditional:
-//
-// for(var word = ''; word.length < 10 * 3; word += 'foo'){}
-// ote: You do not have to overshoot by 1 as with word = Array(11).join('a')
-
-//
-// var word = Array(niveis).join('R$');
-// function gerarPiramide(niveis){
-//   for(var i=0; i<niveis; i++){
-//        for (var i = 1; i < niveis; i++) {
-//             word[i];
-//
-//        }
-//     // return console.log(word);
-//   }
-//   return console.log(word);
-// }

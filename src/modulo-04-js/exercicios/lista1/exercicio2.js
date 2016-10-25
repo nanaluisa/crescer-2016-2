541a066005f2f37deb677963b249b118b5f7e62d
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-10-25 10:51:16 -0200

    feat(exercicio2, exercicio1) : implementar números divisiveis por 3 e 5 ('Diglett dig, trio trio trio') e rename de pasta exercicio1.html para exercicios-lista1.

---
+++ src/modulo-04-js/exercicios/lista1/exercicio2.js
@@ -0,0 +1,24 @@
+// 2. Diglett dig, trio trio trio
+// Escreva uma função diglettDig() que, utilizando console.log, imprime todos os números de 1 até 100, com duas exceções:
+// quando o número for divisível por 3, imprima 'Diglett dig' invés do número, e
+// quando o número for divisível por 5 (e não por 3), imprima 'trio trio trio' invés do número.
+// Após ter essa lógica funcionando, altere a função para imprimir 'Diglett dig, trio trio trio' para números que são
+// divisíveis tanto por 3 quanto por 5 (e que continue imprimindo 'Diglett dig' ou 'trio trio trio' para os números divisíveis
+// apenas por 3 ou 5).
+
+function numeros(){
+  for (var i = 0; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('Diglett dig, trio trio trio');
+    }
+    else if (i % 3 === 0 /*&& i !== 0*/) {
+        console.log('Diglett dig');
+    }
+    else if (i % 5 === 0 && i % 3 !== 0) {
+          console.log('trio trio trio');
+      }
+    else {
+      console.log(i);
+    }
+  }
+}

24083b524545fd76c1ccf863405542a281e5d380
Anna Luísa <annaluisa1703@gmail.com>
2016-10-26 20:56:41 -0200

    refactory(exercicios-lista2) : alterações da aula de reforço.

--- src/modulo-04-js/exercicios/lista2-marvel/exercicio1.js
+++ src/modulo-04-js/exercicios/lista2-marvel/exercicio1.js
@@ -26,66 +26,112 @@ basta procurar no seu array events
 por algum item que tenha 'Civil War' dentro da string.
 */
 
-// for (let prop in arrayDeHerois){
-//     let heroi = arrayDeHerois[prop];
-//     let comics = heroi.comics.items;
-// }
- foraDaGuerraCivil() {
-    this.personagensDaPaz = new Array();
-    for (var prop in this.personagens) {
-      if (object.hasOwnProperty(prop)) {
 
-      }
-    }
-    let ind = 0;
-    //iterando personagens:
-    for (let i = 0; i < this.personagens.length; i++) {
-        for (let j = 0; j < this.personagens[i].events.items.length; j++) {
-            if(this.personagens[i].events.items[j].name !== 'Civil War'){
-                personagensDaPaz.push(this.personagens[i]);
-
-// personagensDaPaz.push(this.personagens[i]);
+  foraDaGuerraCivil() { //2
+        this.personagensDaPaz = new Array();
+        for (let prop in this.personagens) {
+            let heroi = this.personagens[prop];
+            let eventos = heroi.events.items;
+            let heroiDaPaz = true;
+            for (let key in eventos) {
+                let itemEvento = eventos[key];
+                if (itemEvento.name === "Civil War") {
+                    heroiDaPaz = false;
+                }
+            }
+            if (heroiDaPaz) {
+                this.personagensDaPaz.push(this.personagens[prop]);
             }
         }
+        return this.personagensDaPaz;
     }
 
-      return personagensDaPaz;
 
+  /*  03. Mais publicado
+
+    Escreva um método maisPublicado() de instância da classe Herois que retorna
+     o objeto personagem (não é preciso criar classe) que mais possui histórias
+     em quadrinhos publicadas.
+--> Para saber a quantidade de histórias em quadrinhos, utilize a propriedade
+     available de comics. Desconsidere a diferença entre os items dentro de
+     comics e o valor de available, pois tivemos que reduzir a quantidade de
+     comics trazidos na coleção, para ficar mais leve o array.
+  */
+   maisPublicado(){
+    return this.personagens.sort((h1,h2)=>
+      h2.comics.available  - h1.comics.available)[0];
+   }
+
+
+/* 04. Média de páginas
+Escreva uma função mediaPaginas() de instância da classe Herois que retorna a
+média de páginas de todos quadrinhos dos personagens. Utilize a propriedade
+pageCount da comic.+
+*///posso usar for ou o reduce.
+
+    mediaPaginas(){
+    //   let totalPaginas = this.personagens.map(personagem =>
+    //     personagens.comic.items.reduce((acum, comic) => acum + comic.pageCount,0)
+    //   )
+    //   .reduce((acum, paginas) => acum + paginas);
+    //   console.log(totalPaginas);
+    //
+    //   return totalPaginas / this.personagens.length;
+    // }
+  //rever, está dando problema de undefined.
+    let totalPaginas =
+    this.personagens.map(personagem =>
+      personagens.comic.items.reduce((acum, comic) => acum + comic.pageCount,0)
+    )
+    .reduce((acum, paginas) => acum + paginas);
+    console.log(totalPaginas);
+
+    return totalPaginas / this.personagens.length;
   }
+
+
+
+/*05. Longevidade
+Escreva um método seriesPorLongevidade de instância da classe Herois que retorna
+todas as séries dos personagens ordenadas por tempo de atividade descendente,
+ou seja, as séries que ficaram / estão há mais tempo sendo produzidas vêm primeiro.
+Para saber a longevidade de uma série, basta utilizar os campos startYear e endYear da série.*/
+seriesPorLongevidade(){
+  let todasSeries = []
+    //this.personagens.map(heroi => this.personagens.series.items)
+    //this.personagens.map(heroi => this.personagens.series.items)
+
 }
 
 
 
 
 
+/*
+06. Comic mais cara
+Escreva um método comicMaisCara de instância da classe Herois que retorna o objeto comic mais caro entre todos personagens.
+Para descobrir o preço total de um comic, some todo seu array prices.
+*/
 
-// for (let prop in personagens){
-//   let ehDaPaz = (personagens[prop])
-//   personagensDaPaz[let i++] = personagens [];
-// }
-//  return personagensDaPaz;
-// }
-//
+comicMaisCara() {
+   let todasComics = []
+   this.personagens.forEach(heroi => {
+     todasComics = todasComics.concat(heroi.comics.items);
+   });
 
-//exemplos:
-// function ehObjeto(obj) {
-//    return typeof obj === 'object';
-// }
-//
-// function iguais(obj1, obj2) {
-//   if (ehObjeto(obj1) && ehObjeto(obj2)) {
-//      // se tiverem quantidades de campos diferentes, já retorna falso
-//      if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-//        return false;
-//     }
-//      // comparando recursivamente propriedades;
-//      for (let prop in obj1) {
-//        let saoIguais = iguais(obj1[prop], obj2[prop]);
-//        if (!saoIguais) return false;
-//      }
-//      return true;
-//    }
-//
-//  return obj1 === obj2;
-//
-// }
+   let totalizarPrecos = comic => comic.prices.reduce(
+     (acc, objetoPreco) => acc + objetoPreco.price
+   , 0);
+
+   return todasComics.sort(
+     (comic1, comic2) => totalizarPrecos(comic2) - totalizarPrecos(comic1)
+   )[0]
+ }
+
+
+
+
+
+
+
+}//fim da classe.

0c47c9ee02260022282195d3a7ccfdb55446574b
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-10-30 01:47:44 -0200

    feat(TerraMedia): implementar funções e atributos para as classes. Exemplos Inicias de C#.

---
+++ src/modulo-05-dotNet/Projects/ConsoleApplication2/TerraMediaTests/ElfoTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Personagens;
+using TerraMedia.Equipamentos;
+
+namespace TerraMediaTests
+{
+    [TestClass]
+    public class ElfoTest
+    {
+        [TestMethod]
+        public void ElfoNasceom10DeVida()
+        {
+            Elfo elfo = new Elfo();
+           
+            Assert.AreEqual(10, elfo.Vida);
+        }
+
+
+        [TestMethod]
+        public void ElfoAtacaAnaoEEstePerde2DeVida()
+        {
+            var elfo = new Elfo();
+            var anao = new Anao();
+
+            elfo.Atacar(anao);
+
+            Assert.AreEqual(18, anao.Vida);
+        }
+
+
+        [TestMethod]
+        public void ElfoComEspadaTemAtaque5()
+        {
+            var elfo = new Elfo();
+            var espada = new Espada();
+
+            elfo.Equipar(espada);
+
+            Assert.AreEqual(5, elfo.Ataque);
+
+        }
+
+
+
+
+
+    }
+}

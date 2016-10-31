2d064315c3bc1c1a1333e1807577610255130f0a
Anna Luísa <annaluisa1703@gmail.com>
2016-10-31 17:38:34 -0200

    feat(StreetFighterMVC): implementar o Site Street Fighter em MVC.

---
+++ src/modulo-05-dotNet/Projects/StreetFigther.Web/StreetFigther.Web/Controllers/StreetFighterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFigther.Web.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult FichaTecnica()
+        {
+            return View();
+        }
+    }
+}

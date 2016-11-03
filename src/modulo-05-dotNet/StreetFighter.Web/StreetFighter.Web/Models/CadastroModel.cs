6eab3a293611220d7bef406fd132c4e0cb981eb8
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-11-03 00:10:11 -0200

    feat(CadastroModel): implementar CadastroModel e deletar a View de Cadastro.

---
+++ src/modulo-05-dotNet/StreetFighter.Web/StreetFighter.Web/Models/CadastroModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Web.Models
+{
+    public class CadastroModel
+    {
+        [DisplayName("URL da Imagem")]
+        public string Imagem { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [DisplayName("Data de Nascimento")]
+        [Required]
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
+        public int Altura { get; set; }
+
+        [Required]
+        public decimal Peso { get; set; }
+
+        [Required]
+        public int Origem { get; set; }
+
+        [DisplayName("Golpes Especiais")]
+        [Required]
+        public string GolpesEspeciais { get; set; }
+
+        [DisplayName("Personagem Oculto?")]
+        public bool Oculto { get; set; }
+
+    }
+}

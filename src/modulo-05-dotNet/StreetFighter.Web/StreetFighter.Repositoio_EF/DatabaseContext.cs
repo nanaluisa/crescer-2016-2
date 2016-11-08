a5fea7454effc2827d5a5e3923ed66004a872fa8
Anna Luísa <annaluisa1703@gmail.com>
2016-11-08 16:05:41 -0200

    refactory(StreetFighter.Web): implementar camadas.

---
+++ src/modulo-05-dotNet/StreetFighter.Web/StreetFighter.Repositoio_EF/DatabaseContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.Repositoio_EF
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("StreetFighter")
+        {
+
+        }
+
+        public DbSet<Personagem> Personagem { get; set; }
+
+
+        //public DbSet<UserRole> UserRole { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+} 
+
+
+
+
+
+
+
+
+    }
+}

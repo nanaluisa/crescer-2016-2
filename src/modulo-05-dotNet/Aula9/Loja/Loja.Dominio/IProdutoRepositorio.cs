f8e9ea4950c270214252d8b5968832b37429fb07
Anna Luísa <annaluisa1703@gmail.com>
2016-11-09 17:39:09 -0200

    feat(Loja): implementação Incompleta ainda.

---
+++ src/modulo-05-dotNet/Aula9/Loja/Loja.Dominio/IProdutoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Dominio
+{
+    public interface IProdutoRepositorio
+    {
+        Produto BuscarPorNome(string nome);
+    }
+
+}
---
+++ src/modulo-05-dotNet/Aula9/Loja/Loja.Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using Loja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Repositorio
+{
+    public class ProdutoRepositorio : IProdutoRepositorio
+        {
+            public Produto BuscarPorNome(string nome)
+            {
+                using (var context = new ContextoDeDados())
+                {
+                    return context.Produto.FirstOrDefault(u => u.Nome.Equals(nome));
+                }
+            }
+        }
+
+
+}
+

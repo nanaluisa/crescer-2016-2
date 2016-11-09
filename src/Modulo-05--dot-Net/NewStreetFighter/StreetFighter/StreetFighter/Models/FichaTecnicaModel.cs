c24e892671bfbe2cbabc7ccb59ad6210f1dc42f6
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-11-09 11:49:57 -0200

    refactory(NewStreetFighter): começando do zero, para resolver problemas com camadas.

---
+++ src/Modulo-05--dot-Net/NewStreetFighter/StreetFighter/StreetFighter/Models/FichaTecnicaModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Web.Models
+{
+    public class FichaTecnicaModel
+    {
+        public string Imagem { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Altura { get; set; }
+        public double Peso { get; set; }
+        public string Origem { get; set; }
+        public string GolpesEspeciais { get; set; }
+        public bool Oculto { get; set; }
+
+
+
+    }
+}

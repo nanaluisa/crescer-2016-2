d8eed917e8f1a97fcbd61c5adf244bdeaabf58cb
Anna Luísa <annaluisa1703@gmail.com>
2016-11-10 14:12:43 -0200

    feat(ProdutoModel): Pendente.

--- src/modulo-05-dotNet/Aula9/Loja/Loja.Web/Models/ProdutoModel.cs
+++ src/modulo-05-dotNet/Aula9/Loja/Loja.Web/Models/ProdutoModel.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using Loja.Dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Loja.Web.Models
 {
-    public class ProdutoModel
+    public  class ProdutoModel
     {
+
+
+
+
+
+            public ProdutoModel ConverterParaViewModel(this Produto produto)
+            {
+                return new ProdutoModel()
+                {
+                    Nome = produto.Nome
+                };
+            }
+        
+
+
+        
+
+
     }
 }

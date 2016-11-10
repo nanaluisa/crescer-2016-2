8f5b4b9941038ed336ce78f01495ba7a2022941e
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-11-10 11:40:13 -0200

    feat(ProdutoRepositorio, ProdutoRepositorioMock): implementação Pendente de métodos e criar Mock para ProdutoRepositorio.

--- src/modulo-05-dotNet/Aula9/Loja/Loja.Repositorio/ProdutoRepositorio.cs
+++ src/modulo-05-dotNet/Aula9/Loja/Loja.Repositorio/ProdutoRepositorio.cs
@@ -8,7 +8,24 @@ using System.Threading.Tasks;
 namespace Loja.Repositorio
 {
     public class ProdutoRepositorio : IProdutoRepositorio
+    {
+        public void CadastrarProduto(Produto produto)
         {
+            using (var context = new ContextoDeDados())
+            {
+
+            }
+
+                //throw new NotImplementedException();
+         }
+
+        public void EditarProduto(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /*
             public Produto BuscarPorNome(string nome)
             {
                 using (var context = new ContextoDeDados())
@@ -16,8 +33,9 @@ namespace Loja.Repositorio
                     return context.Produto.FirstOrDefault(u => u.Nome.Equals(nome));
                 }
             }
-        }
+        */
 
-
+    }
+    
 }
 

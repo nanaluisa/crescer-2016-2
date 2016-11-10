20ca27da638dfce72293c498e0ce92bba5698ceb
Anna Luísa <annaluisa1703@gmail.com>
2016-11-10 16:46:36 -0200

    feat(telaPrincipal): implementar get api.

--- src/modulo-06-ajax/Marvelflix/Marvelflix/Scripts/app/telaPrincipal.js
+++ src/modulo-06-ajax/Marvelflix/Marvelflix/Scripts/app/telaPrincipal.js
@@ -13,12 +13,23 @@ class TelaPrincipal {
     $('.tela-centralizada').removeClass('tela-centralizada');
     this.$elem.show();  
    
-    marvelflix.render('.tela', 'tela-principal', {
-      chars: [
-        { id: 1, name: 'Iron Man', thumbnail: 'https://cloud.githubusercontent.com/assets/526075/20180159/652c5846-a740-11e6-8b56-19256fcc52c3.png' },
-        { id: 2, name: 'Spider Man', thumbnail: 'https://cloud.githubusercontent.com/assets/526075/20180159/652c5846-a740-11e6-8b56-19256fcc52c3.png' },
-      ]
-    });
+    $.get('/api/herois')
+       .done((res) => {
+           marvelflix.render('.tela', 'tela-principal', {
+               chars:
+
+
+                 /*[
+                 { id: 1, name: 'Iron Man', thumbnail: 'https://cloud.githubusercontent.com/assets/526075/20180159/652c5846-a740-11e6-8b56-19256fcc52c3.png' },
+                 { id: 2, name: 'Spider Man', thumbnail: 'https://cloud.githubusercontent.com/assets/526075/20180159/652c5846-a740-11e6-8b56-19256fcc52c3.png' },
+               ]*/
+           });
+       });
+
+    console.log('renderizar com dados estaticos');
+
+
+   
 
   }
 

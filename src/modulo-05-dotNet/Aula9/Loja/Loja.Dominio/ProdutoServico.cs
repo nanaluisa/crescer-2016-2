ae2c081ec0ab2fbfac243dab71c085d088a0e7ab
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-11-10 00:12:04 -0200

    feat(ProdutoServico):implementar as chamadas de métodos [construtor,  Cadastrar, Editar ].

--- src/modulo-05-dotNet/Aula9/Loja/Loja.Dominio/ProdutoServico.cs
+++ src/modulo-05-dotNet/Aula9/Loja/Loja.Dominio/ProdutoServico.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Loja.Dominio
 {
-    class ProdutoServico
+    public class ProdutoServico
     {
+        private IProdutoRepositorio produtoRepositorio;
+
+        public ProdutoServico(IProdutoRepositorio produtoRepositorio)
+        {
+            this.produtoRepositorio = produtoRepositorio;
+        }
+
+        public void CadastrarProduto(Produto produto)
+        {
+            if (produto.Id == 0)
+            {
+                produtoRepositorio.CadastrarProduto(produto);
+            }
+            else
+            {
+                produtoRepositorio.EditarProduto(produto);
+            }
+        }
+
+
+        /*
+        - Home com listagem de produtos e link de edição
+	                                 + exclusão é bônus
+        - Cadastrar novo produto.
+        */
+
+
+
+
+
+
     }
 }

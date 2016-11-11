009ae1c22feda47a06e5c1af777efed7c97b38b1
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-11-11 11:49:52 -0200

    feat(Marvelflix): implementar Marvelflix.

---
+++ src/Modulo 6 - Ajax/Marvelflix/Marvelflix.Repositorio/Repositorios/HeroisRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using Marvelflix.Dominio;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Marvelflix.Repositorio
+{
+    public class HeroisRepositorio : IHeroisRepositorio
+    {
+        public IEnumerable<Heroi> Todos()
+        {
+            using (var contexto = new ContextoDeDados())
+            {
+                return contexto.Heroi.ToList();
+            }
+        }
+
+        public Heroi PorId(int id)
+        {
+            using (var contexto = new ContextoDeDados())
+            {
+                return contexto.Heroi
+                    .Include(_ => _.Comics)
+                    .SingleOrDefault(_ => _.Id == id);
+            }
+        }
+
+        public void Adicionar(Heroi heroi)
+        {
+            using (var contexto = new ContextoDeDados())
+            {
+                contexto.Heroi.Add(heroi);
+                contexto.SaveChanges();
+            }
+        }
+    }
+}

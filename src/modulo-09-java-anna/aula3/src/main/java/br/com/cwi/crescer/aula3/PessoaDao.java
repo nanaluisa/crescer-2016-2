7fda404c60559384f119150f2cfa4ad1827ea413
Anna Luísa SIlva <annaluisa1703@gmail.com>
2016-11-29 00:28:54 -0200

    fix(aula3): alterar exercícios de aula.

--- src/modulo-09-java-anna/aula3/src/main/java/br/com/cwi/crescer/aula3/PessoaDao.java
+++ src/modulo-09-java-anna/aula3/src/main/java/br/com/cwi/crescer/aula3/PessoaDao.java
@@ -1,18 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.cwi.crescer.aula3;
 
 import br.com.cwi.crescer.aula3.dao.IDao;
-import entity.Pessoa;
+import br.com.cwi.crescer.aula3.entity.Pessoa;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
-import org.eclipse.persistence.sessions.Session;
+import javax.persistence.EntityManager; 
+import org.hibernate.Criteria; 
+import org.hibernate.Session; 
+import org.hibernate.criterion.Example; 
+import static org.hibernate.criterion.MatchMode.ANYWHERE;
+import org.hibernate.criterion.Restrictions; 
+ 
 
 /**
  *
@@ -20,62 +18,59 @@ import org.eclipse.persistence.sessions.Session;
  */
 public class PessoaDao implements IDao<Pessoa, Long> {
 
-    final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("CRESCER");
-    final EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-    @Override
-    public void insert(Pessoa pessoa) {
-        try {
-            entityManager.getTransaction().begin();
-            
-
-            entityManager.persist(pessoa);
-            entityManager.getTransaction().commit();
-
-            
-            
-            
-            
-         //   final Pessoa pessoa = new Pessoa();
-            pessoa.setIdPessoa(1l);
-            pessoa.setNmPessoa("Anna Luisa");
-
-            entityManager.persist(pessoa);
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            entityManager.getTransaction().rollback();
-        }
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void delete(Pessoa t) {
-
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Pessoa find(Long id) {
-
-        //entityManager.find(Pessoa.class, ID);
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<Pessoa> findAll() {
-        return entityManager.createQuery("select p from Pessoa p").getResultList();
-    }
-    
-    public List<Pessoa> findAll(String nome) {
-        //com Hibernate
-        final Session session = entityManager.unwrap(Session.class);
-        final Criteria criteria = session.createCriteria(Pessoa.class);
-        criteria.add(Restrictions.like )
-        
-        /* Com o 
-        final Query query = entityManager.createQuery("select p from Pessoa p where p.nmPessoa = :nmPessoa");
-        query.setParameter("nmPessoa", nmPessoa.toUpperCase());
-        return query.getResultList();*/
-    }
-    
+   final EntityManager entityManager; 
+ 
+    public PessoaDao(EntityManager entityManager) { 
+        this.entityManager = entityManager; 
+    } 
+     
+    @Override 
+    public void insert(Pessoa pessoa) { 
+        try { 
+            entityManager.getTransaction().begin(); 
+            if (pessoa.getIdPessoa() == null) { 
+                entityManager.persist(pessoa); 
+            } else { 
+                entityManager.merge(pessoa); 
+            } 
+            entityManager.getTransaction().commit(); 
+        } catch (Exception e) { 
+            entityManager.getTransaction().rollback(); 
+        } 
+    } 
+ 
+    @Override 
+    public void delete(Pessoa pessoa) { 
+        try { 
+            entityManager.getTransaction().begin(); 
+            entityManager.remove(pessoa); 
+            entityManager.getTransaction().commit(); 
+        } catch (Exception e) { 
+            entityManager.getTransaction().rollback(); 
+        } 
+    } 
+ 
+    @Override 
+    public Pessoa find(Long id) { 
+        return entityManager.find(Pessoa.class, id); 
+    } 
+ 
+    @Override 
+    public List<Pessoa> findAll() { 
+        return entityManager.createQuery("select p from Pessoa p").getResultList(); 
+    } 
+ 
+    public List<Pessoa> findByNome(String nmPessoa) { 
+        final Session session = entityManager.unwrap(Session.class); 
+        final Criteria criteria = session.createCriteria(Pessoa.class); 
+        criteria.add(Restrictions.like("nmPessoa", nmPessoa, ANYWHERE)); 
+        return criteria.list(); 
+    } 
+ 
+    public List<Pessoa> findByPessoa(Pessoa pessoa) { 
+        final Session session = entityManager.unwrap(Session.class); 
+        final Criteria criteria = session.createCriteria(Pessoa.class); 
+        criteria.add(Example.create(pessoa)); 
+        return criteria.list(); 
+    } 
 }
--- src/modulo-09-java-anna/aula3/src/main/java/br/com/cwi/crescer/aula3/Run.java
+++ src/modulo-09-java-anna/aula3/src/main/java/br/com/cwi/crescer/aula3/Run.java
@@ -5,7 +5,9 @@
  */
 package br.com.cwi.crescer.aula3;
 
-import entity.Pessoa;
+
+import br.com.cwi.crescer.aula3.entity.Pessoa;
+//import java.util.ArrayList;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -17,7 +19,35 @@ import javax.persistence.Persistence;
 public class Run {
 
     public static void main(String[] args) {
-        // Criando um EntityManager
+//        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("CRESCER"); 
+//        final EntityManager em = emf.createEntityManager(); 
+// 
+//        final PessoaDao pessoaDao; 
+//        pessoaDao = new PessoaDao(em);
+// 
+//        final Pessoa pessoa = new Pessoa(); 
+////        pessoa.setNmPessoa("Carlos"); 
+// 
+//        final ArrayList<Filho> filhos = new ArrayList<>(); 
+//        pessoa.setFilhos(filhos); 
+//        final Filho filho = new Filho(); 
+//        filho.setNmPessoa("teste 1"); 
+//        filhos.add(filho); 
+//         
+////        for (int i = 0; i < 10; i++) { 
+////            filhos.add(filho); 
+////        } 
+//        pessoaDao.insert(pessoa); 
+// 
+//        pessoaDao.findByPessoa(pessoa).forEach(p -> System.out.println(p.getNmPessoa())); 
+//        em.close(); 
+//        emf.close(); 
+// 
+//    } 
+// 
+//} 
+
+// Criando um EntityManager
         final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("CRESCER");
         final EntityManager entityManager = entityManagerFactory.createEntityManager();
 
